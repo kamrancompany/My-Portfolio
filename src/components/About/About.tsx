b5cc76724f8135ccf19bde6fbdee5c5fc8a4fad6
kamrancompany <132373098+kamrancompany@users.noreply.github.com>
2024-03-19 22:22:56 +0500

    Modify:some modifications

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,5 +1,5 @@
 import { Container } from "./styles";
-import VinayakSingh from "../../assets/VinayakSingh.png";
+import kamran from "../../assets/kamran.jpg";
 import wordpress from "../../assets/wordpress.svg";
 import shopify from "../../assets/shopify.svg";
 import htmlIcon from "../../assets/html-icon.svg";
@@ -90,7 +90,7 @@ export function About() {
       </div>
       <div className="about-image">
         <ScrollAnimation animateIn="fadeInRight" delay={0.20 * 1000}>
-          <img src={VinayakSingh} alt="Vinayak Singh" />
+          <img src={kamran} alt="kamran arshad" />
         </ScrollAnimation>
       </div>
     </Container>
