dcaa2c92d8100b97c10f22701c0ad50db0aa867c
kamrancompany <132373098+kamrancompany@users.noreply.github.com>
2024-03-19 22:25:42 +0500

    Modify:src/components/hero update connection links

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -31,21 +31,21 @@ export function Hero() {
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInUp" delay={1 * 1000}>
       <div className="social-media"><a
-        href="https://www.linkedin.com/in/codevinayak"
+        href="https://www.linkedin.com/in/kamran-arshad-227560274"
         target="_blank"
         rel="noreferrer"
       >
         <img src={linkedin} alt="Linkedin" />
       </a>
         <a
-          href="https://github.com/CodeVinayak/"
+          href="https://github.com/kamrancompany"
           target="_blank"
           rel="noreferrer"
         >
           <img src={githubIcon} alt="GitHub" />
         </a>
         <a
-          href="https://api.whatsapp.com/send/?phone=%2B919630576848&text=Hello+Vinayak"
+          href="https://api.whatsapp.com/send/?phone=%2B923079960816&text=Hello+Kamran"
           target="_blank"
           rel="noreferrer"
         >
