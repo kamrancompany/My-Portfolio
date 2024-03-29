31514a93f4688a3ee37f15f6400218d7b2495917
kamrancompany <132373098+kamrancompany@users.noreply.github.com>
2024-03-29 22:37:57 +0500

    Create: create skills.tsx for constant data

---
+++ src/constants/skills.tsx
@@ -0,0 +1,98 @@
+import mongodb from "../assets/mongodb.svg"
+import reactIcon from "../assets/react-icon.svg";
+import next from "../assets/nextjs-icon-svgrepo-com.svg"
+import redux from "../assets/redux.svg"
+import typescriptIcon from "../assets/typescript-icon.svg";
+import nodeIcon from "../assets/node-icon.svg";
+import expressjs from "../assets/expressjs.svg";
+import htmlIcon from "../assets/html-icon.svg";
+import cssIcon from "../assets/css-icon.svg";
+import boostrapIcon from "../assets/bootstrap-icon.svg";
+import tailwind from "../assets/tailwind-css.svg"
+import jsIcon from "../assets/js-icon.svg";
+export const skills = [
+    {
+        hoverSkills:"MongoDB",
+        img:mongodb,
+        heading:"MongoDB",
+        rating:"8/10",
+        backGround:"#23ce6b",
+    },
+    {
+        hoverSkills:"ReactJs",
+        img:reactIcon,
+        heading:"React Js",
+        rating:"9/10",
+        backGround:"#03bafc",
+    },
+    {
+        hoverSkills:"NextJs",
+        img:next,
+        heading:"Next Js",
+        rating:"7.5/10",
+        backGround:"#03bafc",
+    },
+    {
+        hoverSkills:"Redux",
+        img:redux,
+        heading:"Redx Toolkit",
+        rating:"7.5/10",
+        backGround:"#651af0",
+    },
+    {
+        hoverSkills:"TypeScript",
+        img:typescriptIcon,
+        heading:"Type Script",
+        rating:"7.5/10",
+        backGround:"#1a77f0",
+    },
+    {
+        hoverSkills:"NodeJs",
+        img:nodeIcon,
+        heading:"Node Js",
+        rating:"8/10",
+        backGround:"#05824a",
+    },
+    {
+        hoverSkills:"ExpressJs",
+        img:expressjs,
+        heading:"Express Js",
+        rating:"8/10",
+        backGround:"#23ce6b",
+    },
+    {
+        hoverSkills:"HTML",
+        img:htmlIcon,
+        heading:"HTML",
+        rating:"9.5/10",
+        backGround:"#f54538",
+    },
+    {
+        hoverSkills:"CSS",
+        img:cssIcon,
+        heading:"CSS",
+        rating:"9.5/10",
+        backGround:"#3893f5",
+    },
+    {
+        hoverSkills:"BootStrap",
+        img:boostrapIcon,
+        heading:"BootStrap",
+        rating:"9.5/10",
+        backGround:"#651af0",
+    },
+    {
+        hoverSkills:"TailwindCSS",
+        img:tailwind,
+        heading:"Tailwind CSS",
+        rating:"9.5/10",
+        backGround:"#56c3c7",
+    },
+    {
+        hoverSkills:"JavaScript",
+        img:jsIcon,
+        heading:"JavaScript",
+        rating:"9/10",
+        backGround:"#999403",
+    },
+]
