31a435d7a1cd47c5c6575dd6dcd862bd388865f6
kamrancompany <132373098+kamrancompany@users.noreply.github.com>
2024-03-29 22:44:55 +0500

    Remove commented code

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,31 +1,16 @@
 import React, { useState } from "react";
 import { Container } from "./styles";
 import kamran from "../../assets/kamran.jpg";
-import mongodb from "../../assets/mongodb.svg"
-import expressjs from "../../assets/expressjs.svg"
-import htmlIcon from "../../assets/html-icon.svg";
-import cssIcon from "../../assets/css-icon.svg";
-import jsIcon from "../../assets/js-icon.svg";
-import nodeIcon from "../../assets/node-icon.svg";
-import reactIcon from "../../assets/react-icon.svg";
-import next from "../../assets/nextjs-icon-svgrepo-com.svg"
-import redux from "../../assets/redux.svg"
-import typescriptIcon from "../../assets/typescript-icon.svg";
-import boostrapIcon from "../../assets/bootstrap-icon.svg";
-import tailwind from "../../assets/tailwind-css.svg"
 import ScrollAnimation from "react-animate-on-scroll";
 import { skills } from "../../constants/skills";
-
 export function About() {
   const [hoveredSkill, setHoveredSkill] = useState<string | null>(null);
-
   const handleMouseEnter = (skill: string) => {
     setHoveredSkill(skill);
-  }
-
+  };
   const handleMouseLeave = () => {
     setHoveredSkill(null);
-  }
+  };
   return (
     <Container id="about">
       <div className="about-text">
@@ -34,122 +19,77 @@ export function About() {
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInLeft" delay={0.1 * 1000}>
           <p className="about-para">
-          Welcome to the Digital Playground! I'm Kamran, your go-to maestro of web development magic. With a powerhouse toolkit boasting <span className="skill-span">HTML, CSS, JavaScript, React, Next.js, TypeScript, Node.js, Express.js, Mongo db, ReduxToolkit </span> and more, I'm geared up to transform your online dreams into captivating realities. Let's sculpt your unique online identity, blending innovation with seamless functionality. Elevate your digital presence with me at the helm, where creativity meets cutting-edge technology.
+            Welcome to the Digital Playground! I'm Kamran, your go-to maestro of
+            web development magic. With a powerhouse toolkit boasting{" "}
+            <span className="skill-span">
+              HTML, CSS, JavaScript, React, Next.js, TypeScript, Node.js,
+              Express.js, Mongo db, ReduxToolkit{" "}
+            </span>{" "}
+            and more, I'm geared up to transform your online dreams into
+            captivating realities. Let's sculpt your unique online identity,
+            blending innovation with seamless functionality. Elevate your
+            digital presence with me at the helm, where creativity meets
+            cutting-edge technology.
           </p>
         </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000} style={{ marginTop: "2rem", marginBottom: "2rem" }}>
-        <p className="about-para">
-          Ready to Transform Your Online Presence? Whether it's crafting a brand-new website or giving your current one a stunning makeover, I'm here to turn your vision into a digital masterpiece. Let's collaborate and breathe life into your website dreams! Get in touch today, and let's embark on this exciting journey together towards online success!</p>
+        <ScrollAnimation
+          animateIn="fadeInLeft"
+          delay={0.2 * 1000}
+          style={{ marginTop: "2rem", marginBottom: "2rem" }}
+        >
+          <p className="about-para">
+            Ready to Transform Your Online Presence? Whether it's crafting a
+            brand-new website or giving your current one a stunning makeover,
+            I'm here to turn your vision into a digital masterpiece. Let's
+            collaborate and breathe life into your website dreams! Get in touch
+            today, and let's embark on this exciting journey together towards
+            online success!
+          </p>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInLeft" delay={0.4 * 1000}>
           <h3>Here are my main skills:</h3>
         </ScrollAnimation>
         <div className="hard-skills">
-        {skills.map((data, index) => (
-    <div key={index} className="hability">
-        <ScrollAnimation animateIn="fadeInUp" delay={0.12 * 1000}>
-            {hoveredSkill === data.hoverSkills ? (
-                <div className="card" onMouseLeave={() => handleMouseLeave()}>
-                    <img src={data.img} alt={data.heading} style={{ height: "40px", width: "80px",marginTop:"5px" }} />
-                    <h5 style={{color: data.backGround}}>{data.heading}</h5>
+          {skills.map((data, index) => (
+            <div key={index} className="hability">
+              <ScrollAnimation animateIn="fadeInUp" delay={0.12 * 1000}>
+                {hoveredSkill === data.hoverSkills ? (
+                  <div className="card" onMouseLeave={() => handleMouseLeave()}>
+                    <img
+                      src={data.img}
+                      alt={data.heading}
+                      style={{
+                        height: "40px",
+                        width: "80px",
+                        marginTop: "5px",
+                      }}
+                    />
+                    <h5 style={{ color: data.backGround }}>{data.heading}</h5>
                     <div className="load-bar">
-                        <div className="progress" style={{background: data.backGround}} />
+                      <div
+                        className="progress"
+                        style={{ background: data.backGround }}
+                      />
                     </div>
-                    <h5 style={{color: data.backGround}}>{data.rating}</h5>
-                </div>
-            ) : (
-                <img
+                    <h5 style={{ color: data.backGround }}>{data.rating}</h5>
+                  </div>
+                ) : (
+                  <img
                     src={data.img}
                     alt={data.heading}
                     onMouseEnter={() => handleMouseEnter(data.hoverSkills)}
-                />
-            )}
-        </ScrollAnimation>
-    </div>
-))}
-
-        {/* <div className="hability">
-          <ScrollAnimation animateIn="fadeInUp" delay={0.12 * 1000}>
-            {hoveredSkill === "ReactJs" ? (
-              <div className="card" onMouseLeave={() => handleMouseLeave()}>
-                <img src={reactIcon} alt="react" style={{ height: "40px", width: "80px" }} />
-                <h5>React JS</h5>
-                <div className="load-bar">
-                  <div className="progress" />
-                </div>
-                <h5>8/10</h5>
-              </div>
-            ) : (
-              <img
-                src={reactIcon}
-                alt="MongoDB"
-                onMouseEnter={() => handleMouseEnter("ReactJs")}
-              />
-            )}
-          </ScrollAnimation>
-        </div>
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.13 * 1000}>
-              <img src={reactIcon} alt="React" />
-            </ScrollAnimation>
-          </div> */}
-          {/* <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.13 * 1000}>
-              <img src={next} alt="React" />
-            </ScrollAnimation>
-          </div> */}
-          {/* <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.13 * 1000}>
-              <img src={redux} alt="React" />
-            </ScrollAnimation>
-          </div>
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.14 * 1000}>
-              <img src={typescriptIcon} alt="Typescript" />
-            </ScrollAnimation>
-          </div> */}
-          {/* <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.16 * 1000}>
-              <img src={nodeIcon} alt="Node" />
-            </ScrollAnimation>
-          </div>
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.16 * 1000}>
-              <img src={expressjs} alt="Node" />
-            </ScrollAnimation>
-          </div> */}
-          {/* <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.17 * 1000}>
-              <img src={htmlIcon} alt="Html" />
-            </ScrollAnimation>
-          </div> */}
-          {/* <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.18 * 1000}>
-              <img src={cssIcon} alt="Css" />
-            </ScrollAnimation>
-          </div> */}
-          {/* <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
-              <img src={boostrapIcon} alt="bootstrap" />
-            </ScrollAnimation>
-          </div>
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
-              <img src={tailwind} alt="bootstrap" />
-            </ScrollAnimation>
-          </div>
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
-              <img src={jsIcon} alt="JavaScript" />
-            </ScrollAnimation>
-          </div> */}
+                  />
+                )}
+              </ScrollAnimation>
+            </div>
+          ))}
         </div>
       </div>
       <div className="about-image">
-        <ScrollAnimation animateIn="fadeInRight" delay={0.20 * 1000}>
+        <ScrollAnimation animateIn="fadeInRight" delay={0.2 * 1000}>
           <img src={kamran} alt="kamran arshad" />
         </ScrollAnimation>
       </div>
     </Container>
-  )
+  );
 }
