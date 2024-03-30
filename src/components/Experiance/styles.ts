36fc6a6c83b0e4f574669d39cc9a5ea61c411368
kamrancompany <132373098+kamrancompany@users.noreply.github.com>
2024-03-30 21:33:17 +0500

    Create styles file for experiance components

---
+++ src/components/Experiance/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  h2 {
+    text-align: center;
+    font-size: 4rem;
+    margin-bottom: 3rem;
+    margin-top: 3rem;
+  }
+`;
