b68156e589de02cbe6477adefeae314edd765d94
kamrancompany <132373098+kamrancompany@users.noreply.github.com>
2024-04-01 22:42:30 +0500

    Update Some code and also remove unnecassary bunch of code

--- src/components/Experiance/Experiance.tsx
+++ src/components/Experiance/Experiance.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container } from "./styles";
 import ScrollAnimation from "react-animate-on-scroll";
+import img1011 from "../../assets/1011.jpg";
 export function Experiance() {
   return (
     <Container id="experiance">
@@ -23,44 +24,31 @@ export function Experiance() {
             }}
           >
             <header>
-              <svg
-                width="50"
-                xmlns="http://www.w3.org/2000/svg"
-                role="img"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="#23ce6b "
-                stroke-width="1"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  marginBottom: ".5rem",
+                }}
               >
-                <title>Folder</title>
-                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
-              </svg>
-              <div className="project-links">
-                <a
-                  href="https://kameleon.co.in/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  {/* <img src={externalLink} alt="Visit site" /> */}
-                </a>
+                <img
+                  src={img1011}
+                  style={{
+                    width: "10rem",
+                    height: "8rem",
+                    borderRadius: "12px",
+                  }}
+                />
               </div>
             </header>
             <div className="body">
-              <h3>Kameleon - Cloth Selling Site</h3>
-              <p>
-                This website sells men's cloth products and helps men look their
-                best. It is easy to find what you need, whether it's cloth
-                shirts, pants, or accessories. The site is useful for men who
-                want to look their best and need some help.
+              <h3>1011 Technologies</h3>
+              <p style={{ marginTop: "1rem" }}>
+                I join 1011 technologies software house as a MERN stack
+                developer in december 2023 and Currently i work there. I done
+                lot of project there most of them related to React and Node API.
               </p>
             </div>
-            <footer>
-              <ul className="tech-list">
-                <li>Shopify</li>
-              </ul>
-            </footer>
           </div>
         </ScrollAnimation>
 
